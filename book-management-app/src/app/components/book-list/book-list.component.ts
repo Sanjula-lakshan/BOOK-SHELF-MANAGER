5de009108f77ca32fd4418004fe9a7643ff01d1a
Sanjula-lakshan <sanjularathnamalala2001@gmail.com>
2025-05-23 10:58:36 +0530

    Enhanzer project completed

---
+++ book-management-app/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Book } from '../../models/book.model';
+import { BookService } from '../../services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+  selectedBook: Book | null = null;
+  isEditing = false;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.bookService.getBooks().subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  selectBook(book: Book): void {
+    this.selectedBook = { ...book };
+    this.isEditing = true;
+  }
+
+  deleteBook(id: number): void {
+    if (confirm('Are you sure you want to delete this book?')) {
+      this.bookService.deleteBook(id).subscribe(() => {
+        this.books = this.books.filter(book => book.id !== id);
+      });
+    }
+  }
+
+  cancelEdit(): void {
+    this.selectedBook = null;
+    this.isEditing = false;
+  }
+}
